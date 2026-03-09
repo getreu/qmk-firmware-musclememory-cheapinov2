2da5f84471b7d5277b5bd28439682bc132ad3b38
Jens Getreu <getreu@web.de>
2026-03-09 06:18:20 +0200

    Preplace custom logic with additional tap dance

--- keyboards/cheapino/keymaps/vial/keymap.c
+++ keyboards/cheapino/keymaps/vial/keymap.c
@@ -139,23 +139,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 /**
- * POST-PROCESSING: post_process_record_user
- * Logic that runs AFTER a key press is handled.
- * Specifically used to clear layers (return to base) after ESC key is tapped on non-base layers.
- *
- * This matches musclememory behavior where ESC acts as an "escape to base layer" key.
- */
-void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!record->event.pressed && record->tap.count > 0) {
-        switch (keycode) {
-            case LT(_L6, KC_NO):
-                layer_clear(); // Reset to Base Layer
-                break;
-        }
-    }
-}
-
-/**
  * EEPROM INITIALIZATION - Set Default Combos and Tap Dances
  * This runs when EEPROM is reset (first flash or reset command)
  *
@@ -237,7 +220,7 @@ void eeconfig_init_user(void) {
         td.on_tap = KC_APP;           // Single tap: Context menu
         td.on_hold = KC_APP;          // Hold: Context menu
         td.on_double_tap = TG(_L2);   // Double tap: Toggle Layer 2
-        td.on_tap_hold = KC_APP;      // Tap then hold: Context menu
+        td.on_tap_hold = KC_NO;       // Ignore
         td.custom_tapping_term = 350; // Match musclememory's 350ms tapping term
         dynamic_keymap_set_tap_dance(0, &td);
     }
@@ -246,12 +229,19 @@ void eeconfig_init_user(void) {
         td.on_tap = TO(0);            // Single tap: to base layer
         td.on_hold = KC_APP;          // Hold: Context menu
         td.on_double_tap = TG(_L2);   // Double tap: Toggle Layer 2
-        td.on_tap_hold = KC_APP;      // Tap then hold: Context menu
+        td.on_tap_hold = KC_NO;       // Ignore
         td.custom_tapping_term = 350; // Match musclememory's 350ms tapping term
         dynamic_keymap_set_tap_dance(1, &td);
     }
-
-    // Tap Dance 1-3: Reserved for user customization via Vial GUI
+    {
+        vial_tap_dance_entry_t td = {0};
+        td.on_tap = TO(0);            // Single tap: to base layer
+        td.on_hold = MO(_L6);         // Mouse layer
+        td.on_double_tap = KC_NO;     // Ingore
+        td.on_tap_hold = KC_NO;       // Ignore
+        td.custom_tapping_term = 350; // Match musclememory's 350ms tapping term
+        dynamic_keymap_set_tap_dance(2, &td);
+    }
 #endif
 
     // Reload Vial configuration to activate defaults
@@ -279,49 +269,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, LT(_L6, KC_NO), KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 [_L2] = LAYOUT_split_3x5_3(
-    KC_U,    KC_D,    KC_E,             KC_F,    KC_N,    KC_TRNS, KC_PMNS, KC_P7,   KC_P8,   KC_P9,   KC_P0,
-    KC_PSLS, KC_PAST, LT(_L3,KC_PEQL),  KC_TAB,  KC_BSPC,          KC_NUM,  KC_P4,   LT(_L3,KC_P5), KC_P6, KC_PENT,
-    KC_X,    KC_A,    KC_B,             KC_C,    LSFT(KC_SCLN),    KC_PPLS, KC_P1,   KC_P2,   KC_P3,   KC_PDOT,
-    KC_TRNS, LT(_L6, KC_NO), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
+    KC_U,    KC_D,    KC_E,             KC_F,    KC_N,    KC_TRNS, KC_PMNS, KC_P7,   KC_P8,         KC_P9,   KC_P0,
+    KC_PSLS, KC_PAST, LT(_L3,KC_PEQL),  KC_TAB,  KC_BSPC,          KC_NUM,  KC_P4,   LT(_L3,KC_P5), KC_P6,   KC_PENT,
+    KC_X,    KC_A,    KC_B,             KC_C,    LSFT(KC_SCLN),    KC_PPLS, KC_P1,   KC_P2,         KC_P3,   KC_PDOT,
+    KC_TRNS, TD(2), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
 ),
 
 [_L3] = LAYOUT_split_3x5_3(
-    KC_1,           KC_2,           KC_3,             KC_4,           KC_5,           KC_TRNS, KC_6,           KC_7,           KC_8,           KC_9,           KC_0,
+    KC_1,           KC_2,           KC_3,             KC_4,           KC_5,           KC_TRNS, KC_6,            KC_7,            KC_8,          KC_9,            KC_0,
     LSFT_T(KC_ESC), RALT_T(KC_INS), LT(_L3,KC_DEL),   LCTL_T(KC_TAB), LALT_T(KC_BSPC),         LALT_T(KC_LEFT), RCTL_T(KC_DOWN), LT(_L3,KC_UP), RALT_T(KC_RGHT), RSFT_T(KC_ENT),
-    KC_GRV,         KC_MINS,        KC_EQL,           RGUI_T(KC_LBRC), KC_RBRC,                KC_BSLS,        RGUI_T(KC_QUOT), KC_COMM,        KC_DOT,         KC_SLSH,
-    KC_TRNS, LT(_L6, KC_NO), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
+    KC_GRV,         KC_MINS,        KC_EQL,           RGUI_T(KC_LBRC), KC_RBRC,                KC_BSLS,         RGUI_T(KC_QUOT), KC_COMM,       KC_DOT,          KC_SLSH,
+    KC_TRNS, TD(2), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
 ),
 
 [_L4] = LAYOUT_split_3x5_3(
-    KC_F1,          KC_F2,          KC_F3,   KC_F4,           KC_F5,   KC_TRNS, KC_F6,          KC_F7,   KC_F8,   KC_F9,          KC_F10,
-    LSFT_T(KC_F11), RALT_T(KC_F12), KC_F13,  LCTL_T(KC_F14),  KC_F15,           KC_F16,         KC_F17,  KC_F18,  RALT_T(KC_F19), RSFT_T(KC_F20),
-    KC_F21,         KC_F22,         KC_F23,  RGUI_T(KC_F24),  KC_NO,            KC_NO,          KC_NO,   KC_NO,   KC_NO,          KC_NO,
-    KC_TRNS, LT(_L6, KC_NO), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
+    KC_F1,          KC_F2,          KC_F3,   KC_F4,           KC_F5,   KC_TRNS, KC_F6,    KC_F7,   KC_F8,   KC_F9,          KC_F10,
+    LSFT_T(KC_F11), RALT_T(KC_F12), KC_F13,  LCTL_T(KC_F14),  KC_F15,           KC_F16,   KC_F17,  KC_F18,  RALT_T(KC_F19), RSFT_T(KC_F20),
+    KC_F21,         KC_F22,         KC_F23,  RGUI_T(KC_F24),  KC_NO,            KC_NO,    KC_NO,   KC_NO,   KC_NO,          KC_NO,
+    KC_TRNS, TD(2), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
 ),
 
 [_L5] = LAYOUT_split_3x5_3(
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_HOME, KC_DEL,  KC_INS,  KC_END,  KC_BSPC,
     KC_ESC,  KC_INS,  KC_DEL,  KC_TAB,  KC_BSPC,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_PGDN, KC_PGUP, KC_NO,   KC_NO,
-    KC_TRNS, LT(_L6, KC_NO), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
+    KC_TRNS, TD(2), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
 ),
 
 [_L6] = LAYOUT_split_3x5_3(
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     MS_BTN5, MS_BTN1, MS_BTN3, MS_BTN2, MS_BTN4,          MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, MS_BTN1,
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_NO,
-    KC_TRNS, LT(_L6, KC_NO), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
+    KC_TRNS, TD(2), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
 ),
 
 [_L7] = LAYOUT_split_3x5_3(
     QK_CLEAR_EEPROM,   KC_NO,   RGB_VAD, RGB_VAI, RGB_TOG, KC_TRNS, KC_NO,   KC_BRID, KC_BRIU, KC_NO,   KC_NO,
     KC_NO,   KC_NO,   RGB_HUD, RGB_HUI, RGB_MOD,          KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_NO,
     KC_NO,   KC_NO,   RGB_SAD, RGB_SAI, RGB_RMOD,         KC_MPLY, KC_MPRV, KC_MNXT, KC_NO,   KC_NO,
-    KC_TRNS, LT(_L6, KC_NO), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
+    KC_TRNS, TD(2), KC_TRNS,                              KC_TRNS, KC_TRNS, TD(1)
 )
 
 };
